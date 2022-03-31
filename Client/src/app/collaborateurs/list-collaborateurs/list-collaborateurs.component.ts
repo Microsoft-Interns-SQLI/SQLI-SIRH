8466dd00a6c6520d392a0d8c0420c584200dda76
Ilyass Khadem <ikhadem@SMB.SQLI.COM>
2022-03-31 11:16:05 +0000

    pagination added and workin

--- Client/src/app/collaborateurs/list-collaborateurs/list-collaborateurs.component.ts
+++ Client/src/app/collaborateurs/list-collaborateurs/list-collaborateurs.component.ts
@@ -8,7 +8,10 @@ import { CollaboratorsService } from 'src/app/services/collaborators.service';
   styleUrls: ['./list-collaborateurs.component.css']
 })
 export class ListCollaborateursComponent implements OnInit {
-  collaboratorsArray: Collaborator[] = [];
+  collaboratorsArray: Collaborator[][] = [];
+  elementsPerPage: number = 10;
+  currentPagination: number = 0;
+  numberOfPaginations: number[] = [];
 
   constructor(private service: CollaboratorsService) { }
 
@@ -18,8 +21,16 @@ export class ListCollaborateursComponent implements OnInit {
 
   collaboratorsServiceMap(): void {
     this.service.getCollaboratorsList().subscribe((data) => {
-      this.collaboratorsArray = data;
+      for (let i = 0; i < data.length; i += this.elementsPerPage) {
+        const chunk = data.slice(i, i + this.elementsPerPage);
+        this.collaboratorsArray.push(chunk);
+      }
+      this.numberOfPaginations = Array(Math.ceil(data.length / this.elementsPerPage)).fill(0).map((x,i)=>i);
     });
   }
-
+  changePagination(i: number) {
+    if (i < 0 || i >= this.collaboratorsArray.length)
+      return;
+    this.currentPagination = i;
+  }
 }
