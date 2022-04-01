4f78bd61bcde9f2294fb4bf6f11252ff4773303e
hamzaelaissaoui <hamza.elaissaoui099@gmail.com>
2022-04-01 15:24:25 +0000

    update

--- Client/src/app/auth/signin/signin.component.ts
+++ Client/src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void { }
+  forms!:FormGroup;
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    this.initForm();
+   }
+
+  onSubmit():void{
+    console.log(this.forms.controls);
+  }
+
+  initForm():void{
+    this.forms = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.pattern("[A-Za-z0-9._-]+@[A-Za-z0-9._-]+\\.[a-z]{2,3}")]),
+      "password": new FormControl(null, [Validators.required, Validators.min(6)])
+    });
+  }
 }
