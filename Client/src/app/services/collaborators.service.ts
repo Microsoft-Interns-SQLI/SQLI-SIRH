f123251b07eafab9bdc8f0ffb828ae623136085e
hamzaelaissaoui <hamza.elaissaoui099@gmail.com>
2022-04-08 09:38:40 +0000

    update impor_collab

--- Client/src/app/services/collaborators.service.ts
+++ Client/src/app/services/collaborators.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpEvent, HttpEventType, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Collaborator } from '../Models/Collaborator';
@@ -13,43 +13,54 @@ export class CollaboratorsService {
     readonly myUrl: string = `${environment.URL}api/Collaborateurs`;
     paginatedResult: PaginatedResults<Collaborator[]> = new PaginatedResults<Collaborator[]>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     getCollaboratorsList(itemsPerPage?: number, page?: number) {
         let params = new HttpParams();
         if (page != null && itemsPerPage != null) {
             params = params.append('pageNumber', page.toString());
-            params  = params.append('pageSize', itemsPerPage.toString());
+            params = params.append('pageSize', itemsPerPage.toString());
         }
-        return this.http.get<any>(this.myUrl, {observe: 'response', params}).pipe(
+        return this.http.get<any>(this.myUrl, { observe: 'response', params }).pipe(
             map(response => {
                 this.paginatedResult.result = response.body;
                 if (response.headers.get('Pagination') != null) {
-                    this.paginatedResult.pagination = JSON.parse(response.headers.get('Pagination')||'');
+                    this.paginatedResult.pagination = JSON.parse(response.headers.get('Pagination') || '');
                 }
                 return this.paginatedResult;
             })
         );
     }
 
-    getCollaboratorByMatricule(id:number|string): Observable<Collaborator> {
-        return this.http.get<any>(this.myUrl + "/" + id, {responseType: 'json'});
+    getCollaboratorByMatricule(id: number | string): Observable<Collaborator> {
+        return this.http.get<any>(this.myUrl + "/" + id, { responseType: 'json' });
     }
 
     addCollaborator(collabToAdd: any) {
         return this.http.post(this.myUrl, collabToAdd);
     }
 
-    updateCollaborator(id: number|string, data: any) {
+    updateCollaborator(id: number | string, data: any) {
         return this.http.put(this.myUrl + `/${id}`, data);
     }
 
-    deleteCollaborator(id: number|string) {
+    deleteCollaborator(id: number | string) {
         return this.http.delete(this.myUrl + `/${id}`);
     }
 
-    importCollaborateurs(file:FormData){
-        return this.http.post<FormData>(this.myUrl+"/import", file,
-            {reportProgress: true, observe:'events'})
+    importCollaborateurs(file: FormData): Observable<HttpEvent<any>>{
+        return this.http.post<any>(this.myUrl + "/import", file,
+            {
+                reportProgress: true,
+                observe: 'events'
+            }).pipe(
+                catchError(this.handleError)
+            )
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        if (error.status === 0 || error.status === 500)
+            return throwError(() => "Something went wrong!");
+        return throwError(() => error.error);
     }
 }
