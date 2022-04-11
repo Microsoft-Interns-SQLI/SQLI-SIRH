be791b77b3bb8eef0e0a792c48e099fc155f974b
hamzaelaissaoui <hamza.elaissaoui099@gmail.com>
2022-04-11 12:34:08 +0000

    ajouté du compte rendu  à l'importation

---
+++ API_MySIRH/DTOs/ImportResult.cs
@@ -0,0 +1,12 @@
+﻿namespace API_MySIRH.DTOs
+{
+    public class ImportResult : Dictionary<string,int>
+    {
+        public ImportResult()
+        {
+            this.Add("AddingCollab", 0);   
+            this.Add("ExistsCollab", 0);
+
+        }
+    }
+}
