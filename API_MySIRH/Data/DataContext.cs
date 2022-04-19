fbdd84c3e6164475c8027554a148c683f4f01051
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-04-19 11:00:32 +0000

    Added Modes to DbContext

--- API_MySIRH/Data/DataContext.cs
+++ API_MySIRH/Data/DataContext.cs
@@ -30,7 +30,7 @@ namespace API_MySIRH.Data
         public DbSet<Collaborateur> Collaborateurs { get; set; }
         public DbSet<SkillCenter> SkillCenters { get; set; }
         public DbSet<Dashboard> Dashboards { get; set; }
-
+        public DbSet<ModeRecrutement> ModesRecrutements { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
