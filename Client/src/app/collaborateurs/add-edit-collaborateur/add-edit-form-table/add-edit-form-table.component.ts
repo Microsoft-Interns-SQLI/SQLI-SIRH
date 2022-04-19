f34676400a6ad321f9cdc667f0de1fd41f2fa134
Ilyass Khadem <khadem.ilyass@gmail.Com>
2022-04-19 10:30:58 +0000

    data now bound to mdm in collab form

--- Client/src/app/collaborateurs/add-edit-collaborateur/add-edit-form-table/add-edit-form-table.component.ts
+++ Client/src/app/collaborateurs/add-edit-collaborateur/add-edit-form-table/add-edit-form-table.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Collaborator } from 'src/app/Models/Collaborator';
+import { Niveaux } from 'src/app/Models/nivaux';
+import { MdmService } from 'src/app/services/mdm.service';
+import { SelectInputData, SelectInputObject } from './_form_inputs/select-input/select-input';
 
 @Component({
   selector: 'app-add-edit-form-table',
@@ -11,11 +14,25 @@ export class AddEditFormTableComponent implements OnInit {
   @Input() collab!: Collaborator;
   @Input() formGroup!: FormGroup;
 
-  constructor() {
+  civiliteData: any = new SelectInputData();
+  niveauxData: any = new SelectInputData();
+  postesData: any = new SelectInputData();
+
+  constructor(private service: MdmService) {
 
   }
 
   ngOnInit(): void {
+    this.civiliteData.data = [
+      new SelectInputObject('M', 'Mr.'),
+      new SelectInputObject('F', 'Mme.')
+    ]
+    this.service.getNiveaux().subscribe(res => {
+      this.niveauxData.data = res.map(obj => new SelectInputObject(obj.id, obj.name));
+    })
+    this.service.getPostes().subscribe(res => {
+      this.postesData.data = res.map(obj => new SelectInputObject(obj.id, obj.name));
+    })
   }
 
 }
--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
-  production: true,
-  URL: `https://sqli-sirh-backend.herokuapp.com/`
-};
+// export const environment = {
+//   production: true,
+//   URL: `https://sqli-sirh-backend.herokuapp.com/`
+// };
