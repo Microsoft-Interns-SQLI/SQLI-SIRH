2a6771e3c108bef22d050afaef2b9e8b1446d37c
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-05-10 14:29:07 +0100

    Return Button Added

--- Client/src/app/collaborateurs/add-edit-collaborateur/add-edit-collaborateur.component.ts
+++ Client/src/app/collaborateurs/add-edit-collaborateur/add-edit-collaborateur.component.ts
@@ -1,6 +1,11 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { delay } from 'rxjs';
 import { routes } from 'src/app/app-routing.module';
@@ -46,34 +51,56 @@ export class AddEditCollaborateurComponent implements OnInit {
   initForm(): void {
     console.log(this.collab);
     this.formGroup = this.fb.group({
-      civilite: [this.collab.civilite, [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      civilite: [
+        this.collab.civilite,
+        [Validators.required, Validators.minLength(1), Validators.maxLength(1)],
+      ],
       nom: [this.collab.nom, Validators.required],
       prenom: [this.collab.prenom, Validators.required],
       username: [''],
-      matricule: [this.collab.matricule, [Validators.required, Validators.minLength(5), Validators.maxLength(8)]],
+      matricule: [
+        this.collab.matricule,
+        [Validators.required, Validators.minLength(5), Validators.maxLength(8)],
+      ],
       emailPersonnel: [this.collab.emailPersonnel, Validators.email],
       email: [this.collab.email, Validators.email],
       phonePersonnel: [this.collab.phonePersonnel],
       phoneProfesionnel: [this.collab.phoneProfesionnel],
-      dateNaissance: [this.datepipe.transform(this.collab.dateNaissance, 'yyyy-MM-dd'), Validators.required],
+      dateNaissance: [
+        this.datepipe.transform(this.collab.dateNaissance, 'yyyy-MM-dd'),
+        Validators.required,
+      ],
       lieuNaissance: [this.collab.lieuNaissance],
       nationnalite: [this.collab.nationnalite],
       numCin: [this.collab.numCin],
       adresse: [this.collab.adresse],
-      dateEntreeSqli: [this.datepipe.transform(this.collab.dateEntreeSqli, 'yyyy-MM-dd')],
-      dateSortieSqli: [this.datepipe.transform(this.collab.dateSortieSqli, 'yyyy-MM-dd')],
-      modeRecrutement: [this.collab.modeRecrutement ? this.collab.modeRecrutement.id : ''],
+      dateEntreeSqli: [
+        this.datepipe.transform(this.collab.dateEntreeSqli, 'yyyy-MM-dd'),
+      ],
+      dateSortieSqli: [
+        this.datepipe.transform(this.collab.dateSortieSqli, 'yyyy-MM-dd'),
+      ],
+      modeRecrutement: [
+        this.collab.modeRecrutement ? this.collab.modeRecrutement.id : '',
+      ],
       niveau: [this.collab.niveau ? this.collab.niveau.id : ''],
       poste: [this.collab.poste ? this.collab.poste.id : ''],
       situationFamiliale: [this.collab.situationFamiliale],
-      dateDebutStage: [this.datepipe.transform(this.collab.dateDebutStage, 'yyyy-MM-dd')],
-      datePremiereExperience: [this.datepipe.transform(this.collab.datePremiereExperience, 'yyyy-MM-dd')],
+      dateDebutStage: [
+        this.datepipe.transform(this.collab.dateDebutStage, 'yyyy-MM-dd'),
+      ],
+      datePremiereExperience: [
+        this.datepipe.transform(
+          this.collab.datePremiereExperience,
+          'yyyy-MM-dd'
+        ),
+      ],
       // diplomes: [this.collab.diplomes]
-    })
+    });
   }
 
   saveCollaborator(): void {
-    let message = "";
+    let message = '';
     if (!this.formGroup.valid) {
       this.formGroup.markAllAsTouched();
       return;
@@ -82,20 +109,27 @@ export class AddEditCollaborateurComponent implements OnInit {
     if (this.collab_id) {
       this.sevice
         .updateCollaborator(this.collab_id, result)
-        .subscribe((res) => {
-        });
-      message = this.collab.prenom + " " + this.collab.nom + " a été modifier avec success";
-      this.toastServise.showToast("success", message);
+        .subscribe((res) => {});
+      message =
+        this.collab.prenom +
+        ' ' +
+        this.collab.nom +
+        ' a été modifier avec success';
+      this.toastServise.showToast('success', message);
     } else {
       this.sevice.addCollaborator(result).subscribe((res) => {
         let collaborator: any = res;
-        message = this.collab.prenom + " " + this.collab.nom + " a été ajouter avec success";
-        this.toastServise.showToast("success", message);
+        message =
+          this.collab.prenom +
+          ' ' +
+          this.collab.nom +
+          ' a été ajouter avec success';
+        this.toastServise.showToast('success', message);
         setTimeout(() => {
-          routes
+          routes;
           // window.location.href = `/addEditcollaborateur/${collaborator.id}`;
           this.router.navigate(['/addEditcollaborateur', collaborator.id]);
-        }, 2000)
+        }, 2000);
       });
     }
   }
@@ -126,7 +160,8 @@ export class AddEditCollaborateurComponent implements OnInit {
     this.collab.poste.id = this.formGroup.value.poste;
     this.collab.situationFamiliale = this.formGroup.value.situationFamiliale;
     this.collab.dateDebutStage = this.formGroup.value.dateDebutStage;
-    this.collab.datePremiereExperience = this.formGroup.value.datePremiereExperience;
+    this.collab.datePremiereExperience =
+      this.formGroup.value.datePremiereExperience;
     // this.collab.diplomes = this.formGroup.value.diplomes;
     let res: CollabAddUpdate = new CollabAddUpdate();
     res.id = this.collab.id;
@@ -163,6 +198,10 @@ export class AddEditCollaborateurComponent implements OnInit {
     res.modeRecrutementId = this.formGroup.value.modeRecrutement;
     console.log(res);
     return res;
+  }
+
+  navigateBack() {
+    this.router.navigate(['/collaborateurs']);
   }
 
   calculateYeas(year: any): any {
