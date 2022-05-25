17383f1b15369275f1e48e56313da8abca1f2933
Hicham O-Sfh <Hicham.O.Sfh@gmail.com>
2022-05-25 12:38:29 +0000

    add vars to env.prod

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  URL: `https://sqli-sirh-backend.herokuapp.com/`
+  URL: `https://sqli-sirh-backend.herokuapp.com/`,
+  status: [
+    { id: 1, name: 'A faire' },
+    { id: 2, name: 'Fait' }
+  ]
 };
