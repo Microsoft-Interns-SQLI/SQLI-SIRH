dc36bac16c293779ccad658ecf51627cc89d86f3
Hicham O-Sfh <Hicham.O.Sfh@gmail.com>
2022-05-31 16:42:43 +0000

    UI retouche : fix responsive table-list-collabs filters

--- Client/src/app/collaborateurs/header/header.component.ts
+++ Client/src/app/collaborateurs/header/header.component.ts
@@ -1,60 +1,59 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgSelectConfig } from '@ng-select/ng-select';
 import { Niveau, Poste } from 'src/app/Models/MdmModel';
-import {MdmService} from 'src/app/services/mdm.service';
+import { MdmService } from 'src/app/services/mdm.service';
 
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  selected:number= 10;
-  value:string = "";
+  selected: number = 10;
+  value: string = "";
   @Output() pageSize = new EventEmitter<number>();
   @Output() search = new EventEmitter<string>();
-  
-  chosenPost!:number;
-  selectedPostes : number[]=[];
-  postes!:Poste[];
-  chosenNiveau!:number;
-  selectedNiveaux : number[]=[];
-  niveaux!:Niveau[];
+
+  chosenPost!: number;
+  selectedPostes: number[] = [];
+  postes!: Poste[];
+  chosenNiveau!: number;
+  selectedNiveaux: number[] = [];
+  niveaux!: Niveau[];
   @Output() postesId = new EventEmitter<number[]>();
   @Output() niveauxId = new EventEmitter<number[]>();
-  
-  constructor(private mdmService:MdmService) { }
+
+  constructor(private mdmService: MdmService) { }
 
   ngOnInit(): void {
-    this.mdmService.getAll("Postes").subscribe((data)=>{
+    this.mdmService.getAll("Postes").subscribe((data) => {
       console.log(data);
       this.postes = data;
     });
-    this.mdmService.getAll("Niveaux").subscribe((data)=>{
+    this.mdmService.getAll("Niveaux").subscribe((data) => {
       console.log(data);
       this.niveaux = data;
     });
   }
 
-  onSelect(){
+  onSelect() {
     this.pageSize.emit(this.selected);
   }
 
-  onSearchChange(){
+  onSearchChange() {
     this.search.emit(this.value);
   }
 
-  
-  onPostesChange(postId:number){
-    this.selectedPostes=[]
+
+  onPostesChange(postId: number) {
+    this.selectedPostes = []
     this.selectedPostes.push(postId);
     this.postesId.emit(this.selectedPostes);
   }
 
-  onNiveauxChange(niveauId:number){
-    this.selectedNiveaux=[]
+  onNiveauxChange(niveauId: number) {
+    this.selectedNiveaux = []
     this.selectedNiveaux.push(niveauId);
     this.niveauxId.emit(this.selectedNiveaux);
   }
