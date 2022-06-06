ebf22559a7d5140a95c7a8c1946ca2dda4c1c384
Ilyass Khadem <khadem.ilyass@gmail.Com>
2022-06-06 14:20:49 +0100

    Demission has it's own endpoints now need some cleaning

---
+++ API_MySIRH/Services/DemissionService.cs
@@ -0,0 +1,49 @@
+﻿using API_MySIRH.DTOs;
+using API_MySIRH.Entities;
+using API_MySIRH.Helpers;
+using API_MySIRH.Interfaces;
+using AutoMapper;
+
+namespace API_MySIRH.Services
+{
+    public class DemissionService : IDemissionService
+    {
+        private readonly IDemissionRepository _demissionRepository;
+        private readonly IMapper _mapper;
+
+        public DemissionService(IDemissionRepository demissionRepository, IMapper mapper)
+        {
+            _demissionRepository = demissionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<DemissionDTO?> AddDemission(DemissionDTO demission)
+        {
+            var data = await this._demissionRepository.AddDemission(this._mapper.Map<Demission>(demission));
+            return this._mapper.Map<DemissionDTO>(data);
+        }
+
+        public async Task DeleteDemission(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DemissionDTO?> GetDemissionById(int id)
+        {
+            var data = await this._demissionRepository.GetDemissionByID(id);
+            return this._mapper.Map<DemissionDTO>(data);
+        }
+
+        public IQueryable<Collaborateur> GetDemissions(FilterParams filterParams)
+        {
+            var data = _demissionRepository.GetDemissions();
+            return data.Where(x => x.Demissions.Any()).AsQueryable();
+        }
+
+        public async Task<DemissionDTO?> UpdateDemission(DemissionDTO demission)
+        {
+            var data = await this._demissionRepository.UpdateDemission(this._mapper.Map<Demission>(demission));
+            return this._mapper.Map<DemissionDTO>(data);
+        }
+    }
+}
